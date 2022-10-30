33b050f2977927f5d7b55a0b34deecdb4f8f6dc4
Elia Magris <elia.magris@libero.it>
2022-10-30 11:45:15 +0100

    esercizio completato

--- assets/main.js
+++ assets/main.js
@@ -4,26 +4,32 @@ function startGame(){
 
     const griglia = document.getElementById("griglia");
     
-    griglia.innerHTML ="";
+    griglia.innerHTML = "";
 
     function GridElement(){
+      
       let item = document.createElement('div');
-      item.classList.add('quadrati');
+      if(Number == 100){
+        item.classList.add('quadrati','quadratobase');
+      
+      }else if(Number == 81){
+        item.classList.add('quadrati2','quadratobase');
+      
+      }else{
+        item.classList.add('quadrati3','quadratobase');
+      }
       return item 
     }
-    
 
     for(let i = 0; i < Number; i++) {
         
         let quadrato = GridElement()
         
-        
-
         quadrato.addEventListener('click', function(){
             console.log(this);
             this.classList.toggle('active');
         })
-
+        
         quadrato.innerText = i + 1;
         console.log(quadrato)
 
